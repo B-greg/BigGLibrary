85570d7b871268dac3cc9d8ebaef495d9e8efed9
gregoire <greg@smartsoftasia.com>
2015-02-12 17:21:33 +0700

    Add transition

--- src/main/java/com/smartsoftasia/bigglibrary/Adapter/IRecycleEvent.java
+++ src/main/java/com/smartsoftasia/bigglibrary/Adapter/IRecycleEvent.java
@@ -1,11 +1,16 @@
 package com.smartsoftasia.bigglibrary.Adapter;
 
+import android.view.View;
+
 /**
  * Created by gregoire on 11/11/14.
  */
 public interface IRecycleEvent{
     public interface OnRecycleClick {
         public void onRecycleClick(int position);
+    }
+    public interface OnRecycleViewClick {
+        public void onRecycleViewClick(int position, View view);
     }
     public interface OnLongRecycleClick{
         public void onLongRecycleClick(int position);
